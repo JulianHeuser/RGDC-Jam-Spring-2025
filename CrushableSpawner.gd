7cec9a6a242308b051c73501cac9eaed3188d7ed
Coletronix <colesfirecar@gmail.com>
2025-03-02 10:54:56 -0500

    beginnings of crushable spawner

---
+++ CrushableSpawner.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+@export var spawnable: Crushable
+@export var max_spawnable_allowed: int = 10
+
+
+fun _timer_timeout() -> void:
+	print("timed out")
+
+func _ready() -> void:
+	
+
+func _process() -> void:
+	t
+	pass
