4c76b1e01633f98539488f7e8b1c0f7c78e334b8
Coletronix <colesfirecar@gmail.com>
2025-03-02 12:16:41 -0500

    level 2 controller + factory script

--- Factory.gd
+++ Factory.gd
@@ -7,15 +7,28 @@ extends StaticBody2D
 
 var amt_in_scene: int = 0
 
+var spawnable_inst: Node
+
+
+func notify(node1_rm: Node, node2_rm: Node, node_added: Node) -> void:
+	if node1_rm.type == spawnable_inst.type:
+		amt_in_scene -= 1
+	if node2_rm.type == spawnable_inst.type:
+		amt_in_scene -= 1
+	
+	if node_added.type == spawnable_inst.type:
+		amt_in_scene += 1
+
 func _timer_timeout() -> void:
 	if amt_in_scene < max_spawnable_allowed:
 		# spawn a new spawnable in the parent
-		var spawned: Node = spawnable.duplicate().instantiate()
+		var spawned: Node = spawnable_inst.duplicate()
 		spawned.bigness = spawn_bigness
 		spawned.position = position
 		get_parent().add_child(spawned)
 		amt_in_scene += 1
 
 func _ready() -> void:
+	spawnable_inst = spawnable.instantiate()
 	$Timer.timeout.connect(_timer_timeout)
 	amt_in_scene = 0
--- Level2Controller.gd
+++ Level2Controller.gd
@@ -1,4 +1,23 @@
 extends Node2D
 
 @export var factories : Array[Factory]
+@export var spawn_to_replenish : Array[PackedScene]
+@export var level : int = 2
+
+func _ready() -> void:
+	CrushMgr.set_level(level)
+
+func made_spaghetti() -> void:
+	_spawn_ingredients()
 	
+func _move_to_random_pos(node: Node) -> Node:
+	node.set_position(Vector2(randf_range(int(-get_viewport_rect().size.x/2), int(get_viewport_rect().size.x/2)), randf_range(-get_viewport_rect().size.y/2, get_viewport_rect().size.y/2)))
+	return node
+
+func _spawn_ingredients() -> void:
+	for r : PackedScene in spawn_to_replenish:
+		add_child(_move_to_random_pos(r.instantiate()))
+	
+func notify_factories(node1_rm: Node, node2_rm: Node, node_added: Node) -> void:
+	for factory: Factory in factories:
+		factory.notify(node1_rm, node2_rm, node_added)
