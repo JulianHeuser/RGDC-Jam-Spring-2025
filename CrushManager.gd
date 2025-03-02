cde1bce42911020643ee75add4bf2b9848dd4a70
Julian <julianheuser@outlook.com>
2025-03-01 20:14:54 -0500

    Particles on combine

--- CrushManager.gd
+++ CrushManager.gd
@@ -1,6 +1,8 @@
 class_name CrushManager
 extends Node
 
+var _crush_particle := preload("res://crush_particles.tscn")
+
 #var combine_dict: Dictionary = {
 	#0: load("res://Pasta.tscn"),
 #}
@@ -25,8 +27,12 @@ func determine_combine_result(node1: Crushable, node2: Crushable) -> Node:
 
 func crush_event(node1: Crushable, node2: Crushable) -> void:
 	if node1 != null and node2 != null:
-		var new_node: Node = determine_combine_result(node1, node2)
+		var new_node: Node2D = determine_combine_result(node1, node2)
 		if new_node != null:
 			node1.queue_free()
 			node2.queue_free()
 			get_tree().root.get_node("Level1").add_child(new_node)
+			var new_particle : GPUParticles2D = _crush_particle.instantiate()
+			new_particle.position = new_node.position
+			new_particle.restart()
+			get_tree().root.get_node("Level1").add_child(new_particle)
